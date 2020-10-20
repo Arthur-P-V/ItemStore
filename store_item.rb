d6867a3c6f023391329f44da389708fd61fa905f
Arthur PV <apetersonveatch@gmail.com>
2020-10-20 14:29:44 -0500

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+#item store listings
+spoon1 = { :size => "small", :color => "silver", :price => 5, :use => "playing spoons" }
+spoon2 = { size: "medium", color: "gold", price: 15, use: "eating soup or cereal" }
+spoon3 = { :size => "large", :color => "platinum", price: 30, :use => "digging holes" }
+
+puts "A #{spoon1[:size]} #{spoon1[:color]} spoon at the low price of #{spoon1[:price]} dollars. This spoon is best used for #{spoon1[:use]}."
+puts "A #{spoon2[:size]} #{spoon2[:color]} spoon at the reasonable price of #{spoon2[:price]} dollars. This spoon is best used for #{spoon2[:use]}."
